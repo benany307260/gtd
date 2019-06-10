4248b6ea8671fa2f2c1809a324d681ec0c773c0d
benany307260 <307260621@qq.com>
2019-06-10 19:41:44 +0800

    new

--- src/main/java/com/bentest/spiders/App.java
+++ src/main/java/com/bentest/spiders/App.java
@@ -1,9 +1,15 @@
-package com.montnets.twilio;
+package com.bentest.spiders;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+//开启缓存
+@EnableCaching
+//开启线程
+@EnableScheduling
 public class App {
 
 	public static void main(String[] args) {
