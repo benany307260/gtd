94a9cca39b4b2887513cf6d63566aba08d427252
benany307260 <307260621@qq.com>
2019-06-10 22:03:12 +0800

    new

--- src/main/java/com/bentest/gtd/repository/GtdScheduleRespository.java
+++ src/main/java/com/bentest/gtd/repository/GtdScheduleRespository.java
@@ -1,6 +1,8 @@
 package com.bentest.gtd.repository;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -8,5 +10,5 @@ import org.springframework.stereotype.Repository;
 import com.bentest.gtd.entity.GtdSchedule;
 @Repository
 public interface GtdScheduleRespository extends JpaRepository<GtdSchedule, Integer>, JpaSpecificationExecutor<GtdSchedule> {
-	
+	List<GtdSchedule> getByItemHopeDoneDate(Integer itemHopeDoneDate);
 }
--- src/main/java/com/bentest/std/controller/gtdController.java
+++ src/main/java/com/bentest/std/controller/gtdController.java
@@ -1,17 +1,26 @@
 package com.bentest.std.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bentest.gtd.entity.GtdSchedule;
+import com.bentest.gtd.repository.GtdScheduleRespository;
+import com.bentest.gtd.result.Result;
 
 @RestController
 public class gtdController {
 	
+	@Autowired
+	private GtdScheduleRespository gtdScheduleRespository;
 	
 	
-	
-	@RequestMapping("/au")
-	public Boolean getAu() {
-		return true;
+	@RequestMapping(value = "/v1/getCurrHopeDone")
+	public Result<List<GtdSchedule>> getCurrHopeDone(@RequestBody(required=false) GtdSchedule gtdSchedule) {
+		return Result.success(gtdScheduleRespository.getByItemHopeDoneDate(gtdSchedule.getItemHopeDoneDate()));
 	}
 	
 }
