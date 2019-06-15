b877ae30eeffb463416444b90624de4762c7be33
benany307260 <307260621@qq.com>
2019-06-15 15:38:20 +0800

    n

--- src/main/resources/static/res/js/index.js
+++ src/main/resources/static/res/js/index.js
@@ -76,6 +76,75 @@ function loadCurrHopeDoneList() {
     loadData(path, true, conditionJson, successFunc);
 };
 
+//今天已办
+function loadCurrDoneList() {
+	
+	//var currtime = new Date().Format("yyyyMMddHHmmss");
+	var currDate = new Date().Format("yyyyMMdd");
+	
+    var condition = {
+    		itemHopeDoneDate: currDate,
+    		itemStatus: 100
+    		//pageNum: 1,
+    		//pageSize: 20
+	};
+    
+    var conditionJson = JSON.stringify(condition);
+    
+    var successFunc = function(data) {
+    	if(data.code != 0)
+    	{
+    		return;
+    	}
+		
+    	var ulCurrHopeDone = $("#ulCurrDone");
+    	ulCurrHopeDone.empty();
+		var dataList = data.data;
+		for (var i = 0; i < dataList.length; i++) 
+		{
+			var hideClass = '';
+			// 已完成，不需要显示操作按钮
+			if(dataList[i].itemStatus == 100){
+				hideClass = 'layui-hide';
+			}
+			
+			var html = '<li id="liId-'+dataList[i].id+'" data-itemhopedonedate="'+dataList[i].itemHopeDoneDate+'" >'
+            + '<a href="user/home.html" class="fly-avatar">'
+            + '<img src="https://tva1.sinaimg.cn/crop.0.0.118.118.180/5db11ff4gw1e77d3nqrv8j203b03cweg.jpg" alt="贤心">'
+            + '</a>'
+            + '<h2>'
+            + '<a class="layui-badge">动态</a>'
+            + '<a href="jie/detail.html">'+dataList[i].itemContent+'</a>'
+            + '</h2>'
+            + '<div class="fly-list-info">'
+            + '<a href="user/home.html" link>'
+            + '<cite>贤心</cite>'
+            + '<i class="iconfont icon-renzheng" title="认证信息：XXX"></i>'
+            + '<i class="layui-badge fly-badge-vip">VIP3</i>'
+            + '</a>'
+            + '<span>刚刚</span>'
+            + '<span class="fly-list-kiss layui-hide-xs" title="悬赏飞吻"><i class="iconfont icon-kiss"></i> 60</span>'
+            + '<a href="javascript:void(0);" onclick="finishItem('+dataList[i].id+')" link>'
+            + '<span class="layui-badge fly-badge-accept layui-bg-gray layui-hide-xs '+hideClass+' " >完成</span>'
+            + '</a>'
+            //+ '<button type="button" class="layui-btn layui-btn-xs">一个标准的按钮</button>'
+            + '<span class="fly-list-nums"> '
+            + '<i class="iconfont icon-pinglun1" title="回答"></i> 66'
+            + '</span>'
+            + '</div>'
+            + '<div class="fly-list-badge">'
+            + '</div>'
+            + '</li>';
+				
+			ulCurrHopeDone.append(html);
+		}
+		
+	};
+    
+	var path = "v1/findItemList";
+    loadData(path, true, conditionJson, successFunc);
+};
+
 // 加载未完成
 function loadNotDoneList() {
 	
@@ -179,6 +248,8 @@ function finishItem(id){
 	    loadData(path, false, conditionJson, null);
 	  
 	    loadCurrHopeDoneList();
+	    loadCurrDoneList();
+	    loadNotDoneList();
 	    
 	    layer.close(index);
 	});
